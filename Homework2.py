4774e73457755823d1e0acff6313ce077a922777
Rafokost <rafokost@mail.ru>
2019-09-17 23:14:19 +0400

    initial commit

---
+++ Homework2.py
@@ -0,0 +1,17 @@
+U=input('U is inner energy = ')
+U=int(U)
+
+P=input('P is gas pressure = ')
+P=int(P)
+
+V=input('V is volume of gas = ')
+V=int(V)
+
+print('H=U+P*V')
+H=U+P*V
+H=str(H)
+print('the value of H is',H)
+H=int(H)
+
+print(bool(H))
+
