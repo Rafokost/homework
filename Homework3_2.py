4766d43c2321261ff31771ccf927819d98514d69
Rafokost <rafokost@mail.ru>
2019-09-20 01:06:20 +0400

    initial commit

---
+++ Homework3_2.py
@@ -0,0 +1,11 @@
+#I want the programm to check if the input is int or str and start to run program but I cant do it for now
+
+electrycity=input("Is there electrycity in the house? = ")
+
+electrycity=="no" and print("take out the fruits from the fridge")
+
+fridge_content=input("How many things in the fridge? = ") 
+
+fridge_content=int(fridge_content)
+
+(electrycity=='yes' and fridge_content<=1) and print("Needs more, go the market!")
