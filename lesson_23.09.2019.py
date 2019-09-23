ee582aa0830e2884857ea9cf6cc00b24264058c4
Rafokost <rafokost@mail.ru>
2019-09-23 20:13:40 +0400

    initial commit

---
+++ lesson_23.09.2019.py
@@ -0,0 +1,5 @@
+Somebodys_name = input('Tell your name ')
+Somebodys_age = input('Tell your birth year = ')
+Somebodys_age = int(Somebodys_age)
+x=Somebodys_age+100-2019
+print('you will be 100 years old after',x)
