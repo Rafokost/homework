bf831edfc1df949ce41134a7eae559942700238e
Rafokost <rafokost@mail.ru>
2019-09-25 20:25:51 +0400

    many examples in one file in classes work

---
+++ lesson_25.09.2019.py
@@ -0,0 +1,36 @@
+'''Somebodys_name = input('Tell your name ')
+Somebodys_age = input('Tell your birth year = ')
+Somebodys_age = int(Somebodys_age)
+x=Somebodys_age+100-2019
+print('you will be 100 years old after',x)'''
+
+'''x=int(input('the first edge = '))
+y=int(input('the second edge = '))
+z=int(input('the third edge = '))
+
+(((x+y>z and x+z>y) and y+z>x) and print('tringle is possible')) or ((x+y<=z or x+z<=y or y+z<=x) and print('tringle is impossible'))'''
+
+
+'''x = int(input("input the first number "))
+y = int(input("input the second number "))
+z = int(input("input the third number "))
+
+
+if x %2 ==0 and y %2==0 and z %2==0:
+	print("the whole input numbers are even")
+
+if x %2 !=0 or y %2!=0 or z %2!=0:
+	print('not all the input numbers are even',x,y,z)'''
+
+weather = input('weather condition ')
+if weather == 'cold':
+	mydress = input('how to dressed ')
+
+if weather == 'cold':
+	if mydress == 'short':
+		print('wear the jacket')
+	else:
+	 	print('you feel good')
+else:
+	print('there is sunshine weather outside')
+
