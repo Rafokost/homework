a8ac7721c9705461c3fabceed7047fe72cfaa244
Rafokost <rafokost@mail.ru>
2019-09-27 19:24:12 +0400

    add some for function exlenation

--- lesson_25.09.2019.py
+++ lesson_25.09.2019.py
@@ -20,7 +20,7 @@ if x %2 ==0 and y %2==0 and z %2==0:
 	print("the whole input numbers are even")
 
 if x %2 !=0 or y %2!=0 or z %2!=0:
-	print('not all the input numbers are even',x,y,z)'''
+	print('not all the input numbers are even',x,y,z)
 
 weather = input('weather condition ')
 if weather == 'cold':
@@ -34,4 +34,29 @@ if weather == 'cold':
 	 	
 else:
 	print('there is sunshine weather outside')
+
+number = int(input('the number: '))
+if number<0:
+	print('the number is negative')
+elif number == 0:
+	print('the number is zero')
+else:
+	print('the number is positive')
+
+
+temp = int(input('temp of frozen cell: '))
+if temp**2//2 <= 40:
+	power = temp*100-142
+	print('needed input power in Watts is: ',power)
+elif temp**2//2 >= 70:
+	power = temp*100//4
+	print('there is no auxilary power',power)
+else:
+	print('Karno cycle is working')'''
+
+for i in range(100,5,-7):
+	print(i)
+
+
+
 
