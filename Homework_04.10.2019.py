3f34dc368abf99feb0a75576960783f8eff048a0
Rafokost <rafokost@mail.ru>
2019-10-06 23:11:15 +0400

    Add some code using try/except

--- Homework_04.10.2019.py
+++ Homework_04.10.2019.py
@@ -1,5 +1,5 @@
 # Kilometers to miles convert program with a little bit difference program runing steps
-'''def kilotomiles(number):
+def kilotomiles(number):
 	number = number*0.62
 	return number
 number = int(input("Input kilometers: "))
@@ -22,28 +22,56 @@ def celtofar(temp):
 	temp = temp*1.8+32
 	print(temp, "fahrenheit")
 temp = int(input("Input temperature in celsius: "))
-(celtofar(temp))'''
+(celtofar(temp))
 
 #word counter in sentence
-
-
 def wordcounter(sent):
-	count = int()
-	b = str()
-	letter = int()
-	probel = int()
-	for i in sent:
-		if i !=  " ":			
-			letter+=1
-		pass
-		probel += 1
-		if probel - letter == 1:
-			count += 1 
-	 
+	count = 0
+	j = sent[0]
+	for i in sent[1:len(sent)]:
+		if i != " " and j == " ":
+			#print(i, "start")
+			pass
+		if i == " " and j != " ":
+			#print(i, "end")
+			count += 1
+		j = i
 
+	if sent.endswith(" ") == False:
+		return count + 1
+	else:
+		return count
 
-	return letter, probel, count
-
-sent = input("Write a sentence: ")
-
+sent = input("write a sentence: ")
 print(wordcounter(sent))
+
+
+#symbol checker program
+def symbol_checker(sent):
+	print(sent.isalpha())
+
+sent = input("type a string: ")
+symbol_checker(sent)
+
+
+# Try/except understanding program
+try:
+	x = 5
+	#print(int('some_input'))
+	a == 8
+	#5 + str()
+except NameError:
+	print("There is Name error occured")
+except ValueError:
+	print("there is Value error occured")
+except TypeError:
+	print("there is Type error occured")
+except:
+	print("Unknown error")
+else:
+	print("no errors occured")
+finally:
+	print("Code has done running!")
+
+
+
