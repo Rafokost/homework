6f741828939922bbedbb7f768df976bc2a579f17
Rafokost <rafokost@mail.ru>
2019-10-09 19:55:48 +0400

    Add some list functions

--- lesson_25.09.2019.py
+++ lesson_25.09.2019.py
@@ -341,4 +341,22 @@ print("Your surname is " + ui[1])
 
 nested_tuple = ((1,2), (3,4), (5,6))
 print(nested_tuple[0])
-print(nested_tuple[0][0])
+print(nested_tuple[0][0], nested_tuple[1][0])
+
+
+my_list = [1, 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21 ,22,23]
+
+my_list.remove(23)
+print(my_list, ": After removing 23")
+
+my_list.sort()
+print(my_list," :After sorting")
+
+my_list.reverse()
+print(my_list," :After reversing")
+
+my_list.pop()
+print(my_list, " : poping")
+
+del my_list[-5:]
+print(my_list, ": After deleting the last five items")
